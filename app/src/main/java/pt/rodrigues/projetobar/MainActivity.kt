e8731ade4fb21e6c562540a975447ad2acff78f1
17rodr1gues <82651137+17rodr1gues@users.noreply.github.com>
2022-05-19 16:44:21 +0100

    Aprovação do Projeto

---
+++ app/src/main/java/pt/rodrigues/projetobar/MainActivity.kt
@@ -0,0 +1,11 @@
+package pt.rodrigues.projetobar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
